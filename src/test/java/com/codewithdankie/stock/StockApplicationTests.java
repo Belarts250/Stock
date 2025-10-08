5e941f740aa98c49d31922c0388bcffa955b763b
Igihozo Belise <igihozobelise6@gmail.com>
2025-10-08 13:28:33 +0200

    first commit

---
+++ src/test/java/com/codewithdankie/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithdankie.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
