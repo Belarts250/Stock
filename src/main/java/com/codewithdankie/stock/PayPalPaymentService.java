2e922a59af269ee6d784458d338818f5040b0323
Igihozo Belise <igihozobelise6@gmail.com>
2025-10-13 16:57:38 +0200

    external configurations

--- src/main/java/com/codewithdankie/stock/PayPalPaymentService.java
+++ src/main/java/com/codewithdankie/stock/PayPalPaymentService.java
@@ -1,12 +1,31 @@
 package com.codewithdankie.stock;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@Service("paypal")
+import java.util.List;
+
+//@Service("paypal")
 public class PayPalPaymentService implements  PaymentService {
+
+    @Value("${paypal.apiUrl}")
+    private String apiUrl;
+
+    @Value("${paypal.enabled}")
+    private boolean enabled;
+
+    @Value("${paypal.timeout:3000}")
+    private int timeout;
+
+    @Value("${paypal.supported-currencies}")
+    private List<String> supportedCurrencies;
     @Override
     public void processPayment(double amount) {
         System.out.println("PAYPAL");
+        System.out.println("API URL: "+ apiUrl);
+        System.out.println("Enabled: "+ enabled);
+        System.out.println("Timeout: " + timeout);
+        System.out.println("Currencies: " + supportedCurrencies);
         System.out.println("Amount: " + amount);
     }
 }
--- src/main/java/com/codewithdankie/stock/StripePaymentService.java
+++ src/main/java/com/codewithdankie/stock/StripePaymentService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Primary
-@Service("stripe")
+//@Primary
+//@Service("stripe")
 public class StripePaymentService implements PaymentService {
 
     @Value("${stripe.apiUrl}")
